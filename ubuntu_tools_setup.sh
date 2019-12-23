50c4cf573788da19e7c43ca5ab5c4b4e6c77db03
Logan <r07525074@ntu.edu.tw>
2019-12-23 20:41:42 +0800

    Add Chinese(Chewing)

--- ubuntu_tools_setup.sh
+++ ubuntu_tools_setup.sh
@@ -6,12 +6,12 @@
 ## Vim
 echo -e "--\e[1;95mInstalling vim \e[0m" # 1:Bold, 95:Light magenta
 sudo apt --assume-yes install vim
-echo -e "--\e[1;92mVim installation completed\e[0m" #92:Light green
+echo -e "--\e[1;92mVim installation complete \e[0m" #92:Light green
 
 ## Terminator
 echo -e "--\e[1;95mInstalling terminator \e[0m" # 1:Bold, 95:Light magenta
 sudo apt-get --assume-yes install terminator
-echo -e "--\e[1;92mTerminator installation completed \e[0m" #92:Light green 
+echo -e "--\e[1;92mTerminator installation complete \e[0m" #92:Light green 
 
 ## Google
 echo -e "--\e[1;95mInstalling terminator \e[0m" # 1:Bold, 95:Light magenta
@@ -19,7 +19,12 @@ sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.li
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install google-chrome-stable
-echo -e "--\e[1;92mTerminator installation completed \e[0m" #92:Light green 
+echo -e "--\e[1;92mTerminator installation complete \e[0m" #92:Light green 
+
+## Chinese(Chewing)
+echo -e "--\e[1;95mInstalling Chinese(Chewing) \e[0m" # 1:Bold, 95:Light magenta
+sudo apt --assume-yes install ibus-chewing
+echo -e "--\e[1;92mChinese(Chewing) installation complete\e[0m" #92:Light green
 
 ## Done
-echo -e "--Ubuntu tools setup completed\e[0m"
+echo -e "--Ubuntu tools setup complete \e[0m"
